91986253b83aa22e0cfaf943e3df1bc5ed02a4e9
Zen <zen@tenmax.io>
2019-09-23 01:45:32 +0800

    GO doesn't has default value in struct, create a constructor function for both Sheep and Grass

--- sheepeatgrass/world/grass.go
+++ sheepeatgrass/world/grass.go
@@ -5,9 +5,27 @@ import (
 )
 
 //Grass - a grass
+//go:generate stringer -type=Grass
 type Grass struct {
+	Life `default:"{\"lifePoint\":3}"`
+	name string
 }
 
 func (g *Grass) Act() {
-	fmt.Println("I am Grass")
+	fmt.Printf("I am %T\n", g)
+}
+
+func (g *Grass) GetHealth() int {
+	return g.lifePoint
+}
+
+func (g *Grass) GetName() string {
+	return "🌱" + g.name
+}
+
+/**
+ * Constructor of Grass
+ */
+func NewGrass(name string, pos Point2D) *Grass {
+	return &Grass{Life{lifePoint: 3, pos: pos}, name}
 }
--- sheepeatgrass/world/sheep.go
+++ sheepeatgrass/world/sheep.go
@@ -6,10 +6,29 @@ import (
 
 //Sheep - a sheep
 type Sheep struct {
+	Life `default:"{\"lifePoint\":5}"`
+	name string
+}
+
+//Constructor of Sheep
+func NewSheep(name string, pos Point2D) *Sheep {
+	return &Sheep{
+		Life{lifePoint: 5, pos: pos}, name}
 }
 
 func (s *Sheep) Act() {
 	// c.lifePoint - DAILY_CONSUME
+	fmt.Printf("I am %T\n", s)
+}
 
-	fmt.Println("I am Sheep")
+func (s *Sheep) GetHealth() int {
+	return s.lifePoint
+}
+
+func (s *Sheep) GetName() string {
+	return "🐏" + s.name
+}
+
+func (s *Sheep) Move(dir Direction) error {
+	return s.pos.Move(dir)
 }
