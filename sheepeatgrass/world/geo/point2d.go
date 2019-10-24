d313a7c42a98b725cc31c624dda014773cd02b4e
Zen <zen@tenmax.io>
2019-10-24 10:43:42 +0800

    Point2D validation

--- sheepeatgrass/world/geo/point2d.go
+++ sheepeatgrass/world/geo/point2d.go
@@ -2,6 +2,7 @@ package geo
 
 import (
 	"fmt"
+	"zentest.io/sheepeatgrass/world/config"
 )
 
 /**
@@ -24,7 +25,7 @@ func (p *Point2D) Move(dir Direction) (Point2D, error) {
 	case South:
 		newY = p.Y + 1
 	}
-	if newX < 0 || newY < 0 {
+	if newX < 0 || newY < 0 || newX >= config.Width || newY >= config.Height {
 		return Point2D{}, fmt.Errorf("Invalid Point: [%v,%v]", newX, newY)
 	}
 	return Point2D{newX, newY}, nil
